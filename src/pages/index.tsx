1473eed7f43c89b1c513b51e0f8b37deaef06111
范晓豪 <fanxiaohaohaha@qq.com>
2023-06-20 17:40:40 +0800

    add:typed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
 import Layout from "@/layout";
+import React from 'react';
+import Typed from 'typed.js';
 
 function IndexPage () {
+  const el = React.useRef(null);
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+      typeSpeed: 50,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
       <Layout>
         <div>
-          <h1>Content</h1>
+          {/*打字机*/}
+          <span ref={el}/>
         </div>
       </Layout>
   )
