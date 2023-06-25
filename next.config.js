c0ec091b22857b95e405d89df819a2c0e62b657d
范晓豪 <fanxiaohaohaha@qq.com>
2023-06-25 17:49:07 +0800

    新增全局变量

--- next.config.js
+++ next.config.js
@@ -1,32 +1,30 @@
 const path = require('path');
-const {PHASE_PRODUCTION_BUILD} = require('next/constants');
-const globalConst = require('./config/globalConst.ts')
+const globalConst = require('./config/globalConst.ts');
 
-module.exports = (phase) => {
-  const isProd = phase === PHASE_PRODUCTION_BUILD;
-  console.log(isProd)
-  const config = {
+const isProd = process.env.NODE_ENV === 'production';
+
+const config = {
+  env: {
     staticPageGenerationTimeout: 90,
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'src/assets/scss')],
-    },
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-
-      return config;
-    },
-  };
-
-  if (isProd) {
-    // 在生产环境下的配置
-    config.env = globalConst.development
-  } else {
-    // 在开发环境下的配置
-    config.env = globalConst.production
-  }
-
-  return config;
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/assets/scss')],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
+
+if (isProd) {
+// 在生产环境下的配置
+  config.env = globalConst.development;
+} else {
+// 在开发环境下的配置
+  config.env = globalConst.production;
+}
+
+module.exports = config;
